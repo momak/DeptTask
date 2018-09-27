9ce272b554dd7d55378c7c3d92b1a66745d61e43
Bojan Bogdanovski <freeboy_bb@yahoo.co.uk>
2018-09-27 23:07:49 +0200

    Add project files.

---
+++ DeptTask/wwwroot/PageScripts/Home/Home.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-full-width",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
+
+    toastr["warning"]("My name is Inigo Montoya. You killed my father. Prepare to die!");
+    toastr.info('Are you the 6 fingered man?');
+
+    hideProgress();
+    $.ajax({
+        url: "https://api.openaq.org/v1/countries",
+        dataType: 'json',
+        type: 'GET',
+        success: function (response) {
+            debugger 
+            toastr.info(response);
+        },
+        error: function (response) {
+            toastr["error"](JSON.parse(response.responseText).message);
+            console.log(response);
+            return false;
+        },
+        beforeSend: function () {
+            showProgress();
+        },
+        complete: function () {
+            hideProgress();
+        }
+    });
+});
